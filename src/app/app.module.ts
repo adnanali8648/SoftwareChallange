ca73250b1ba99a2ebe7778306dc410adf78f4bbd
adnanali8648 <adnan.ali8648@gmail.com>
2022-03-13 15:16:18 +0100

    UI implement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import {MatTableModule} from '@angular/material/table';
+import { AppRoutingModule, ArrayOfComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+// import { ChocolateListComponent } from './chocolate-list/chocolate-list.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+// import { ChocolateDetailComponent } from './chocolate-detail/chocolate-detail.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArrayOfComponents 
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
