adb983719d1fdb31d049e09825542074dbd804b8
adnanali8648 <adnan.ali8648@gmail.com>
2022-03-13 17:04:32 +0100

    logic added

--- src/app/chocolate-detail/chocolate-detail.component.ts
+++ src/app/chocolate-detail/chocolate-detail.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import{ChocolateListComponent} from '../chocolate-list/chocolate-list.component';
+import {Nutrition,Chocolate, ChocolateService,Price } from '../chocolate.service';
+import { ActivatedRoute } from '@angular/router';
+
+// interface sortedData {
+//   id: number;
+//   name: string;
+//   brand: string;
+//   currency: string;
+//   prices: Price[];
+//   nutrition: Nutrition;
+
+// }
 
 @Component({
   selector: 'app-chocolate-detail',
   templateUrl: './chocolate-detail.component.html',
   styleUrls: ['./chocolate-detail.component.css']
 })
-export class ChocolateDetailComponent implements OnInit {
 
-  constructor() { }
+
+export class ChocolateDetailComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'prices', 'amount', 'shopLink'];
+  // dataSource = ELEMENT_DATA;
+
+  sortedDate: Chocolate[]=[]; 
+  dataSource :Chocolate={};
+  DetailById :Price[][]=[];
+
+  constructor(private route: ActivatedRoute,private dataService: ChocolateService) { }
 
   ngOnInit(): void {
+
+ //console.log  ( this.route.snapshot.params['id']);
+ this.dataService.getUsers().subscribe(data =>{
+
+  let dataById = data.filter(y => {return y.id==this.route.snapshot.params['id']});
+  this.dataSource = Object.assign({}, ...dataById ) 
+  // this.DetailById=this.dataSource.map(y=>y.prices)
+  console.log(dataById,'object')
+
+
+ })
+    
   }
 
 }
--- src/app/chocolate-list/chocolate-list.component.ts
+++ src/app/chocolate-list/chocolate-list.component.ts
@@ -1,15 +1,55 @@
+import { LEADING_TRIVIA_CHARS } from '@angular/compiler/src/render3/view/template';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Chocolate, ChocolateService, Price } from '../chocolate.service';
+import { Router } from '@angular/router'; 
+
+
+ interface sortedData {
+  id: number;
+  name: string;
+  brand: string;
+  currency: string;
+  prices: Price[];
+  average:number;
+}
+
 
 @Component({
   selector: 'app-chocolate-list',
   templateUrl: './chocolate-list.component.html',
   styleUrls: ['./chocolate-list.component.css']
 })
+
+
 export class ChocolateListComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  sortedDate: sortedData[]=[];
+  id:number=0;
+  constructor(private router: Router,private dataService: ChocolateService) { 
+    
   }
 
+  getId(a:number){
+    this.id=a;
+    // this.router.navigateByUrl('/details');
+    this.router.navigate(['/details',this.id]); 
+    console.log(this.router.navigate(['/details',this.id]))
+  }
+
+
+  averagePrice(array:any) {  return array.map((a:any) => a.price).reduce((b:any,c:any)=> b+c)  / array.length }
+  sortPrice(array:any) {return array.sort((a:any, b:any) =>a.price-b.price); }
+  ngOnInit() {
+    
+     this.dataService.getUsers().subscribe(data=>{
+     this.sortedDate = data.map((item:any) => (
+      {
+        ...item,
+        prices: this.sortPrice(item.prices), //item.prices.sort((a:any,b:any) =>(a.price-b.price)),
+        average : this.averagePrice(item.prices) //item.prices.map((a:any) => a.price).reduce((b:any,c:any)=> b+c)  / item.prices.length 
+      }
+    ))  
+    });
+  }
 }
